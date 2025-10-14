9009473b9c879d28472136d369ffe6b4efce5286
Jakob Reh <jakob.reh@gmx.de>
2025-10-14 18:50:56 +0200

    Startumgebung

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,33 @@
+import {
+    Map,
+    MapTileLayer,
+    MapZoomControl,
+    MapLocateControl,
+} from "@/components/ui/map"
+import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+export default async function Dashboard() {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if (!session) {
+        redirect("/sign-in");
+    }
+
+    return (
+        <div className="font-sans flex flex-col min-h-screen">
+            <main className="flex flex-col gap-2 items-start flex-grow w-full">
+                <Map center={[50.733602, 7.093770]} className="flex-grow w-full">
+                    <MapTileLayer />
+                    <div className="absolute top-1 left-1 z-1000 grid gap-1">
+                        <MapLocateControl className="static" />
+                        <MapZoomControl className="static" />
+                    </div>
+                </Map>
+            </main>
+        </div>
+    );
+}
---
+++ lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient("mongodb://localhost:27017/jakobscode");
+export const db = client.db();
+
+export const auth = betterAuth({
+    database: mongodbAdapter(db, {
+        client
+    }),
+    socialProviders: {
+        google: { 
+            accessType: "offline", 
+            prompt: "select_account consent", 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
+});

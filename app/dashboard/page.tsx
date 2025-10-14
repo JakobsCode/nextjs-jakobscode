2cc7736478d0a33ca1f226c65414cf76773bd7a5
Jakob Reh <jakob.reh@gmx.de>
2025-10-15 00:10:04 +0200

    Cool!

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,6 +7,9 @@ import {
 import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import { ButtonGroup } from "@/components/ui/button-group";
+import { Button } from "@/components/ui/button";
+import { User } from "lucide-react";
 
 export default async function Dashboard() {
     const session = await auth.api.getSession({
@@ -24,6 +27,18 @@ export default async function Dashboard() {
                     <MapTileLayer />
                     <div className="absolute top-1 left-1 z-1000 grid gap-1">
                         <MapLocateControl className="static" />
+                        <ButtonGroup
+                            orientation="vertical"
+                            aria-label="Zoom controls"
+                            className="top-1 left-1 z-1000 h-fit static">
+                            <Button
+                                type="button"
+                                size="icon-sm"
+                                variant="secondary"
+                                className="border">
+                                <User />
+                            </Button>
+                        </ButtonGroup>
                         <MapZoomControl className="static" />
                     </div>
                 </Map>

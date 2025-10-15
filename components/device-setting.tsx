2083c4d34c2ae6874bfda55e70ee08f82b6587c0
Jakob Reh <jakob.reh@gmx.de>
2025-10-15 23:36:58 +0200

    API Key kann erstellt werden!

--- components/device-setting.tsx
+++ components/device-setting.tsx
@@ -11,8 +11,44 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+    Field,
+    FieldDescription,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+    FieldLegend,
+    FieldSet,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+import * as z from "zod"
+import { authClient } from "@/lib/auth-client"
+
+const formSchema = z.object({
+    name: z.string().min(2).max(100),
+})
+
 export function DeviceSettings() {
     const [open, setOpen] = React.useState(false)
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    })
+
+    async function onSubmit(data: z.infer<typeof formSchema>) {
+        const { data: apiKeyData, error } = await authClient.apiKey.create({
+            name: data.name,
+            expiresIn: null,
+        });
+        console.log({ apiKeyData, error });
+    }
+
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
@@ -21,18 +57,39 @@ export function DeviceSettings() {
                 </Button>
             </DialogTrigger>
             <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Set up your device</DialogTitle>
-                    <DialogDescription>
-                        Create a new device to start tracking it.
-                    </DialogDescription>
-                </DialogHeader>
-                <main>
-                    Gerät
-                </main>
-                <DialogFooter>
-                    <Button type="submit">Confirm</Button>
-                </DialogFooter>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <DialogHeader>
+                        <DialogTitle>Set up your device</DialogTitle>
+                        <DialogDescription>
+                            Create a new device to start tracking it.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <main className="py-3">
+                        <Controller
+                            name="name"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid}>
+                                    <FieldLabel htmlFor={field.name}>Device Name</FieldLabel>
+                                    <Input
+                                        {...field}
+                                        id={field.name}
+                                        aria-invalid={fieldState.invalid}
+                                        placeholder="GPS Tracker"
+                                        autoComplete="off"
+                                    />
+                                    <FieldDescription>
+                                        Provide a concise title for your device.
+                                    </FieldDescription>
+                                    {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                                </Field>
+                            )}
+                        />
+                    </main>
+                    <DialogFooter>
+                        <Button type="submit" loading={form.formState.isSubmitting}>Confirm</Button>
+                    </DialogFooter>
+                </form>
             </DialogContent>
         </Dialog>
     )
